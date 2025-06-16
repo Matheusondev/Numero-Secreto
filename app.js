63b8cdaf11948a32a7659ccef02d376f075a09db
Matheus <matheusmichelin890@proton.me>
2025-06-16 16:04:17 -0300

    git

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
       let listaDeNumeros = []
-      let numeroLimite = 1000
+      let numeroLimite = 100
         let numeroSecreto = gerarNumeroAleatorio()
         let tentativas = 1
         function exibirTextoNaTela(tag, texto) {
@@ -16,11 +16,11 @@
 }
   function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto')
-        exibirTextoNaTela('p', 'Escolha um número entre 1 e 1000')
+        exibirTextoNaTela('p', 'Escolha um número entre 1 e 100')
 
   }
         exibirTextoNaTela('h1', 'Jogo do número secreto')
-        exibirTextoNaTela('p', 'Escolha um número entre 1 e 1000')
+        exibirTextoNaTela('p', 'Escolha um número entre 1 e 100')
 
         function verificarChute(){
           let chute = document.querySelector ('input').value
